a145345d7ede17730b3794209c59be4a52ca7f86
jannis.grebien <jannisgrebien@web.de>
2020-10-18 18:03:26 +0200

    adding database connection

--- controllers/register.js
+++ controllers/register.js
@@ -5,30 +5,28 @@ const handleRegister = (db, bcrypt) => (req, res) =>{
     }
     const hash = bcrypt.hashSync(password)
 
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return db('users')
-                .returning('*')
-                .insert({
-                email: loginEmail[0],
-                name : name,
-                joined: new Date()
-            }).then(user => {
-                res.json(user[0]);
-            })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return db('users')
+            .returning('*')
+            .insert({
+            email: loginEmail[0],
+            name : name,
+            joined: new Date()
+        }).then(user => {
+            res.json(user[0]);
+        })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
     .catch(err => res.status(400).json('unable to register'))
-
 }
 
 
